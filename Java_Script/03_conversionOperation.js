a7df4b11ed0a79bc6ca98edd9fb7d1e224b92be1
PyDevData <68914471+kartik27chauhan@users.noreply.github.com>
2024-10-21 07:10:50 +0000

    Conversion of Data Types

---
+++ Java_Script/03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score = "33"
+//console.log(typeof score);
+
+//To chnage the string in number
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+let age = "26abc"
+//console.log("Type of Age is:",typeof age)
+
+//To chnage the string in number
+let InNumber = Number(age) 
+/*
+It gonna change the string into number but when you print variable
+the output will be NaN =>Not a Number
+*/
+//console.log(typeof InNumber)
+//console.log(InNumber)
+
+//if the value in variable is assigned null
+let s = null
+//console.log(typeof s)
+
+let c = Number(s)
+//console.log("Type of c :",typeof c)
+//console.log(c)
+
+//if the value in variable is undefined 
+let q = undefined
+//console.log(typeof q)
+
+let w = Number(q)
+//console.log("Type of w :",typeof w)
+//console.log(w)
+//if the value in variable is boolean 
+//console.log("For Boolean value")
+let e = true
+//console.log(typeof e)
+
+let f = Number(e)
+//console.log("Type of f :",typeof f)
+//console.log(f)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggesdIn = 0
+let booleanIsLoggedIn = Boolean(isLoggesdIn)
+//console.log("Type of :", typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1 => true
+// "" => false
+// "kartik" => true
+
+//Convert in string
+
+let someNumber = 45
+console.log(typeof someNumber)
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
