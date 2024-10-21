20428049870acdb00adfbbaeeabeebc70b7fa8b9
PyDevData <68914471+kartik27chauhan@users.noreply.github.com>
2024-10-21 06:31:40 +0000

    Data Types in JS

---
+++ Java_Script/02_Data_types.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+let name = "Kartik" 
+let age = 26       
+let isLoggedIn = false 
+
+//PRIMITIVE DATA TYPES:
+
+// number => to power 53
+// bigint
+// string => ""
+// null => standalone value
+// Undefined => 
+// symbol => unique
+// boolean => true/false
+
+// OBJECT:
+console.log(typeof "kartik")
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
+
+
+
+
+
+
