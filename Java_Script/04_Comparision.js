b7df0b051dfb37d821d7f43079849cda5fbbb034
PyDevData <68914471+kartik27chauhan@users.noreply.github.com>
2024-10-22 05:10:01 +0000

    Memory concept added in JS

--- Java_Script/04_Comparision.js
+++ Java_Script/04_Comparision.js
@@ -1,25 +1,50 @@
-console.log(2 > 1);
-console.log(2 >= 1)
-console.log(2 == 1);
-console.log(2 != 1);
+//console.log(2 > 1);
+//console.log(2 >= 1)
+//console.log(2 == 1);
+//console.log(2 != 1);
 
 
 
-console.log("2" > 1); 
-console.log("02" > 1);
+//console.log("2" > 1); 
+//console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined >= 0)
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined >= 0)
 
 // === Check data types also
 
-console.log("2" === 2);
-console.log(0 == false);
+//console.log("2" === 2);
+//console.log(0 == false);
+
+// +++++++++++++++++++++++ Memory +++++++++++++++++++++++++++++++++++++++++++
+
+// Stack Memory (Primitive)
+
+let myinstaname = "Kartik._lifts_"
+
+let anothername = myinstaname
+anothername = "Blue_smoke"
+console.log(myinstaname);
+console.log(anothername);
+
+//HEap Memory (Non-Primitive)
+let userOne = {
+    gmail : "xyz@gmail.com",
+    upi : "swi@sbi-345"
+}
+let userTwo = userOne
+
+userTwo.gmail = "abc@gmail.com"
+
+console.log(userOne.gmail)
+console.log(userTwo.gmail);
+
+
 
 
 
