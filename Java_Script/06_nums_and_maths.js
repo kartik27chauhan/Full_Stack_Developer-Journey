2dce9446618169fa547c4c73c5337cf61095d643
kartik27chauhan <kartikch863@gmail.com>
2024-11-14 15:30:57 +0530

    Math & Numbers

---
+++ Java_Script/06_nums_and_maths.js
@@ -0,0 +1,70 @@
+// const score = 600
+// console.log(score);
+
+
+
+// const balance = new Number(100)
+// console.log(balance)
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+// 
+// const number_1  = 122.54
+// console.log(number_1.toPrecision(3));
+
+// const number_2 = 1000000
+// console.log(number_2.toLocaleString());
+
+// Indian Values
+// const number_3 = 1000000
+// console.log(number_3.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++++  MATHS +++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// --> abs() Change the negative value to positive only negative
+// console.log(Math.abs(-4));
+
+// --> around() the value will be round off
+// console.log(Math.round(4.6));
+// To choose the upper value you use -->ceil
+
+// console.log(Math.ceil(6.4));
+
+// To choose the lower value we use -->floor
+// console.log(Math.floor(4.9));
+
+// --> Random value will always between 0-1
+// console.log(Math.random());
+// console.log(Math.random());
+
+// console.log(Math.random()*10);
+
+// To get greater value above 1
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
