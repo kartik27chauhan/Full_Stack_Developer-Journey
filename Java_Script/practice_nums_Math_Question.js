70cc540de888dac9a1cf91998c7c2cad3ca8999a
kartik27chauhan <kartikch863@gmail.com>
2024-11-14 17:20:09 +0530

    Practice Question of Maths

---
+++ Java_Script/practice_nums_Math_Question.js
@@ -0,0 +1,28 @@
+// Use Math.random to generate a random number between 1 and 10.
+// const min = 1
+// const max = 10
+// console.log(Math.random() * (max - min +1 ) + min);
+
+
+//Q2--> Round the number 4.7 down to the nearest integer using Math.floor
+console.log(Math.floor(4.7));
+
+// Q3--> Find the result of raising 3 to the power of 4 using Math.pow.
+console.log(Math.pow(4,3));
+
+// q4--> Use Math.round to round 5.67 to the nearest integer.
+console.log("Round Value of 5.67 is:",Math.round(5.67));
+
+
+// Q5--> Find the absolute value of -15. Use Math.abs.
+console.log(Math.abs(-15));
+
+// Q6--> Use Math.ceil to round the number 6.3 up to the nearest integer.
+console.log(Math.ceil(6.3));
+
+
+
+
+
+
+
