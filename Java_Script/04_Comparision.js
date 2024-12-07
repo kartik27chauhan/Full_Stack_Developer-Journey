c7091ef0147a60626ba7da0c42fbf2254725cbc9
PyDevData <68914471+kartik27chauhan@users.noreply.github.com>
2024-12-07 16:16:54 +0000

    Comparision Operators

--- Java_Script/04_Comparision.js
+++ Java_Script/04_Comparision.js
@@ -5,44 +5,35 @@
 
 
 
-//console.log("2" > 1); 
-//console.log("02" > 1);
+// console.log("2" > 1); 
+// console.log("02" > 1); 
+/*
+In the above the JS automatically converted string into number it is wrong (Data type should be same)
+*/  
 
-//console.log(null > 0);
-//console.log(null == 0);
-//console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+// Here in (3) the comparision operator convert null to number it as 0
 
-//console.log(undefined == 0);
-//console.log(undefined > 0);
-//console.log(undefined >= 0)
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0)
 
 // === Check data types also
 
-//console.log("2" === 2);
-//console.log(0 == false);
+console.log("2" === 2);
+console.log(0 == false);
 
-// +++++++++++++++++++++++ Memory +++++++++++++++++++++++++++++++++++++++++++
+let a = "20"
+let b = "5"
+let c = a > b
+console.log(c);
 
-// Stack Memory (Primitive)
 
-let myinstaname = "Kartik._lifts_"
 
-let anothername = myinstaname
-anothername = "Blue_smoke"
-console.log(myinstaname);
-console.log(anothername);
 
-//HEap Memory (Non-Primitive)
-let userOne = {
-    gmail : "xyz@gmail.com",
-    upi : "swi@sbi-345"
-}
-let userTwo = userOne
 
-userTwo.gmail = "abc@gmail.com"
-
-console.log(userOne.gmail)
-console.log(userTwo.gmail);
 
 
 
