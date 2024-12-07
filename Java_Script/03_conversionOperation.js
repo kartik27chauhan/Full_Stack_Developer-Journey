345f07d0f642cc71b970ffad2a1e89937a4b7dcf
PyDevData <68914471+kartik27chauhan@users.noreply.github.com>
2024-12-07 07:52:05 +0000

    Operations added

--- Java_Script/03_conversionOperation.js
+++ Java_Script/03_conversionOperation.js
@@ -1,45 +1,45 @@
 let score = 33
-console.log(typeof(score));
+// console.log(typeof(score));
 
 // If the value is in string
 
 let score1 = "33"
-console.log(typeof(score1));
+// console.log(typeof(score1));
 
 // Convert the String into Number
 let valueInNumber = Number(score1)
-console.log(valueInNumber);
-console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
 
 // if we have number and string both ex-"123abcd"
 let numberAndString = "123abcd"
-console.log(numberAndString);
-console.log(typeof(numberAndString));
+// console.log(numberAndString);
+// console.log(typeof(numberAndString));
 /*
 Converting numberAndString into Number as its type is String
 */
 let stringToNumber = Number(numberAndString)
-console.log(typeof(stringToNumber));
-console.log(stringToNumber);
+// console.log(typeof(stringToNumber));
+// console.log(stringToNumber);
 
 // if the vaue is null
 let valueNull = null
-console.log(valueNull);
+// console.log(valueNull);
 let nullToNumber = Number(valueNull)
-console.log(nullToNumber);
+// console.log(nullToNumber);
 
 
 // If the value is UNDEFINED
 let valueUndefined = undefined
-console.log(valueUndefined);
+// console.log(valueUndefined);
 let undefinedToNumber = Number(valueUndefined)
-console.log(undefinedToNumber);
+// console.log(undefinedToNumber);
 
 // if the value is BOOLEAN
 let valueBoolean = false
-console.log(valueBoolean);
+// console.log(valueBoolean);
 let booleanToNumber = Number(valueBoolean)
-console.log(booleanToNumber);
+// console.log(booleanToNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -48,7 +48,7 @@ console.log(booleanToNumber);
 // Number to BOOLEAN
 let numberToBoolean = 0
 let booleanValue = Boolean(numberToBoolean)
-console.log(booleanValue);
+// console.log(booleanValue);
 
 // 1 => true ; 0 => false
 // "" => false
@@ -58,8 +58,31 @@ console.log(booleanValue);
 
 let someNumber = 78
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// ****************** Operations ***************************
+let a = 3
+let negValue = -a
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Kartik"
+let str3 = str1 + " " + str2
+// console.log(str3);
+
+// First string and then number  or vice versa
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+
+
+
+
 
 
 
