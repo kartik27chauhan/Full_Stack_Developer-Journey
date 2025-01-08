21aff5dd74e96e76ce7ef3b635858f2a1bf441c5
PyDevData <68914471+kartik27chauhan@users.noreply.github.com>
2025-01-08 13:30:38 +0000

    Date MEthods in JS

--- Java_Script/07_Date_time.js
+++ Java_Script/07_Date_time.js
@@ -38,9 +38,26 @@ There are 9 ways to create a new date object:
 --> JavaScript counts months from 0 to 11:
 */
 
-const d4 = new Date(2018, 12, 24, 10, 33, 30)
-console.log("Specifying a month higher than 11, will not result in an error but add the overflow to the next year:",d4)
-const d5 = new Date(2018, 2, 35, 10, 34, 30)
-console.log("Specifying a day higher than max, will not result in an error but add the overflow to the next month:",d5);
+// const d4 = new Date(2018, 12, 24, 10, 33, 30)
+// console.log("Specifying a month higher than 11, will not result in an error but add the overflow to the next year:",d4)
+// const d5 = new Date(2018, 2, 35, 10, 34, 30)
+// console.log("Specifying a day higher than max, will not result in an error but add the overflow to the next month:",d5);
+
+// ++++++++++++++++++++++++ Date Methods ++++++++++++++++++++++++++++++++++
+// 1--> toString
+const d6 = new Date()
+console.log("when you display a date object in HTML, it is automatically converted to a string with toString() method:", d6.toString());
+
+// 2--> toDateString
+const d7 = new Date()
+console.log("This method converts a date to more readable format:", d7.toDateString());
+
+// 3 --> toUTCString()
+const d8 = new Date()
+console.log("This method converts a date to a string using the UTC standard", d8.toUTCString());
+
+// 4 --> toIOSString()
+const d9 = new Date()
+console.log("This method converts a date to a string using the ISO standard:",d9.toISOString());
 
 
