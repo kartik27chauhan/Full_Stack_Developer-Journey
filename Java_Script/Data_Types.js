2bff47eec9efda45e986db82160d1561fcd17fe2
kartik27chauhan <kartikch863@gmail.com>
2025-02-14 23:33:40 +0530

    JS lectures

---
+++ Java_Script/Data_Types.js
@@ -0,0 +1,25 @@
+//Primitive Types
+    //String - "abcdd"
+    //Number - 1,2,3,4,5
+    //Boolean - True/False
+    //Undefined - undefined
+    //Null - null
+
+let a = undefined
+console.log(typeof(a));
+let b = null;
+console.log(b);
+let c = true
+console.log(typeof(c));
+
+//JS is dynamically typed language
+let d = 10
+d = "Hello"
+console.log(d);
+
+const e = "Kartik"
+e = 87
+console.log(e); //Error
+
+
+
---
+++ Java_Script/variable.js
@@ -0,0 +1,24 @@
+// In JavaScript, a variable is a container for storing data values.
+// You can declare a variable using the var, let, or const keywords.
+
+
+// let: Block-scoped, can be re-assigned but not re-declared within the same scope.
+
+let a = 5
+console.log(a) // 5
+let name = "Kartik" // kartik
+console.log(name);
+let nameLast = "Chauhan" // chauhan
+console.log(nameLast);
+
+// var: Function-scoped or globally-scoped, can be re-assigned and re-declared.
+var b = 10
+console.log(b) // 10;
+
+// const: Block-scoped, cannot be re-assigned or re-declared
+const c = 23
+console.log(c); // 23
+//const c = "Kartik"
+console.log(c); // error
+
+
