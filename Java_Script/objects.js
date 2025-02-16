7b1dd6de0ed275508e7205ceaff0076996332df0
kartik27chauhan <kartikch863@gmail.com>
2025-02-16 23:15:33 +0530

    Objects in JS

---
+++ Java_Script/objects.js
@@ -0,0 +1,26 @@
+// let person = {
+//     name : "kartik",
+//     age: 26,
+//     email: "xyz@gmail.com"
+// }
+// console.log(person);
+
+// This example creates an empty JavaScript object, and then adds 4 properties:
+const person1 = {}
+
+// Adding properties to an object
+// person1.firstName = "Rahul"
+// person1.lastName = "Sharma"
+// person1.age = 26
+// console.log(person1);
+// This example create a new JavaScript object using new Object(), and then adds 4 properties:
+const person3 = new Object()
+person3.firstName = "John"
+person3.lastName = "Doe"
+person3.age = 50
+console.log(person3);
+console.log(person3.age);
+// dot notaion and bracket notation
+console.log(person3["lastName"]);
+
+
