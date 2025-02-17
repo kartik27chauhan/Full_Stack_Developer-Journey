2bdc3d9ec4695b3c13e9b436a1e6778f517cee32
kartik27chauhan <kartikch863@gmail.com>
2025-02-17 23:32:19 +0530

    Operators in JS

---
+++ Java_Script/Operators.js
@@ -0,0 +1,56 @@
+// Arithmatic operators
+let a = 10;
+let b = 5;
+
+// Addition
+let addition = a + b;
+console.log(`Addition: ${addition}`);
+
+// Subtraction
+let subtraction = a - b;
+console.log(`Subtraction: ${subtraction}`);
+
+// Multiplication
+let multiplication = a * b;
+console.log(`Multiplication: ${multiplication}`);
+
+// Division
+let division = a / b;
+console.log(`Division: ${division}`);
+
+// Modulus
+let modulus = a % b;
+console.log(`Modulus: ${modulus}`);
+
+// Exponentiation
+let exponentiation = a ** b;
+console.log(`Exponentiation: ${exponentiation}`);
+
+// Assignment Operators
+// === is used to compare the value and the type of the variable
+// == is used to compare the value of the variable
+let a1 = 6
+let b1 = 3
+console.log(a1 == b1); //false
+console.log(a1 === b1); //false
+let a2 = 5
+let b2 = "5"
+console.log(a2 == b2); //true
+console.log(a2 === b2); //false
+// String Comparison
+
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2;
+console.log(result);
+
+
+
+
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
